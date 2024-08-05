47b8e80768b0c64f288ff407b69ba8c86ae50eb6
候帅 <houshuai0816@126.com>
2024-08-05 22:21:50 +0800

    feat(wasm-example):集成WebAssembly模块并调整Vue项目结构
    - 配置 `.gitignore` 以排除 node_modules 和其他编译/构建文件。- 移除 src/lib.rs 中的加密实现代码，暂时用 stub 替换。
    - 添加完整的前端项目结构，包括 Vite 和 Vue 3 配置。
    - 在 web 目录下添加 wasm 示例应用，包括 WebAssembly 模块的集成。
    - 添加 `.idea` 目录和相关文件以支持 IntelliJ IDEA。
    
    此提交准备了一个完整的前端项目结构，用于演示 WebAssembly 模块与 Vue 3应用的集成。

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,12 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn encrypt_aes(key: &str, iv: &str) -> Result<String, JsValue> {
-    let key = GenericArray::from_slice(key.as_bytes());
-    let iv_bytes = GenericArray::from_slice(iv.as_bytes());
-    let cipher = Aes128::new(&key);
-    let mut block = iv_bytes.clone();
-    cipher.encrypt_block(&mut block);
-    let ciphertext_base64 = encode(&block);
-    Ok(ciphertext_base64)
+    // let key = GenericArray::from_slice(key.as_bytes());
+    // let iv_bytes = GenericArray::from_slice(iv.as_bytes());
+    // let cipher = Aes128::new(&key);
+    // let mut block = iv_bytes.clone();
+    // cipher.encrypt_block(&mut block);
+    // let ciphertext_base64 = encode(&block);
+    // Ok(ciphertext_base64)
+    Ok("Hello".to_string())
 }
---
+++ web/wasm-example/src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import * as wasmModule from './wasm_learn_bg.wasm'
+
+function test(){
+  console.log(wasmModule.encrypt_aes("aG9sZGVyX3Rlc3QxMjM0", "aWZfY29tcGFueV90ZXN0"))
+}
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+    <button @click="test">test</button>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
